42f999229ed5c1060a1496e1bffc4439d4063e74
werdnaknom <andrew.m.monk@gmail.com>
2022-09-07 21:11:19 -0700

    testing send_file

--- app/display_images/routes.py
+++ app/display_images/routes.py
@@ -4,6 +4,8 @@ from . import bp
 from flask import send_from_directory, send_file
 
 
+from pathlib import Path
+
 @bp.route('/download/<path:filename>')
 def display_file(filename):
     return send_from_directory(Config.DATADIRECTORY, filename, as_attachment=True)
@@ -12,4 +14,7 @@ def display_file(filename):
 @bp.route('/display/<path:filename>')
 def load_image(filename):
     print("loading {}".format(filename))
+    p = Path(filename)
+    print(p.exists(), p.resolve())
+    print(Path("//npo/coos").exists(), "NPO COOS")
     return send_file(filename, mimetype='image/fig')
